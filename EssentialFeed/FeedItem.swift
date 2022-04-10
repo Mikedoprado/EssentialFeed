f1ee71126e6e5e751b5841c6b99787f6c3218228
Michael Conchado <mike.doprado78@gmail.com>
2022-04-10 16:01:47 -0500

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Michael Conchado on 10/04/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
